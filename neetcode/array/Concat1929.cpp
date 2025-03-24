bca83250fe197df35662ddaa468486915b53005c
Shravan Bhat <pseudou@Shravans-MacBook-Air.local>
2025-03-23 22:22:35 -0500

    217. Contains Duplicate

--- neetcode/array/Concat1929.cpp
+++ neetcode/array/Concat1929.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-class Solution {
+class Concat1929 {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
         int n = nums.size();
@@ -19,7 +19,7 @@ public:
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    Solution s = Solution();
+    Concat1929 s = Concat1929();
     vector<int> nums = {1,3,2,1};
     vector<int> ans = s.getConcatenation(nums);
     for (int i=0; i< ans.size();i++) {
---
+++ neetcode/array/Dupes217.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<unordered_set>
+
+using namespace std;
+class Dupes217 {
+public:
+    bool containsDupes(vector<int> &a) {
+        unordered_set<int> s;
+
+        for (int i:a) {
+            if (s.find(i) == s.end()) {
+                s.insert(i);
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+    Dupes217 s = Dupes217();
+    vector<int> test = {1,2,3, 1};
+    cout << s.containsDupes(test);
+    return 0;
+}
